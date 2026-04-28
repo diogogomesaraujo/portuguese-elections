d39b6addade8d4fc81dfe8a347ae1c0f6d75c5e0
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-04-28 20:58:55 +0100

    plotting map correctly

--- backend/lib/map.ml
+++ backend/lib/map.ml
@@ -13,7 +13,7 @@ module Map = struct
     ) r) (fetch_single_result c)#get_all
 
   let get_pol_xy pol =
-    List.fold_left
+    let (x, y) = List.fold_left
       (fun (acc_x, acc_y) p' ->
         let x_to_add, y_to_add = List.fold_left
         (fun (x', y') (x_to_add, y_to_add) ->
@@ -21,40 +21,51 @@ module Map = struct
         ) ([],[]) p'
         in (acc_x @ x_to_add, acc_y @ y_to_add)
       ) ([], []) pol
+    in (Array.of_list x, Array.of_list y)
 
-  let get_wkt_xy (pol: wkt_term) =
+  let get_wkt_xy_l (pol: wkt_term) =
     match pol with
     | Polygon p ->
-      get_pol_xy p
+      [get_pol_xy p]
     | Multipolygon p ->
-      List.fold_left (fun (x, y) p' ->
-        get_pol_xy p'
-      ) ([],[]) p
+      List.map (fun p' -> get_pol_xy p') p
 
-  let get_xy pols =
-    let (x, y) = Array.fold_left
-      (fun (acc_x, acc_y) p ->
-        let x_to_add, y_to_add = Array.fold_left
-        (fun (x', y') p ->
-          let (x_to_add', y_to_add') = get_wkt_xy p in
-          (x' @ x_to_add', y' @ y_to_add')
-        ) ([],[]) p
-        in (acc_x @ x_to_add, acc_y @ y_to_add)
-      ) ([], []) pols in
-    (Array.of_list x, Array.of_list y)
+  let get_xy (pols_l_l: wkt_term array array) =
+    Array.fold_left (fun acc p_l ->
+      acc @ (Array.fold_left (
+        fun acc' p -> acc' @ get_wkt_xy_l p
+      ) [] p_l)
+    ) [] pols_l_l
 
   let get_xy_min_max (x, y) =
     let min_max a = Array.fold_left (
-      fun (ma, mi) a' ->
-        (max ma a', min mi a')
-    ) (0., Float.max_float) a in
-    let (max_x, min_x) = min_max x in
-    let (max_y, min_y) = min_max y in
-    (max_x, min_x, max_y, min_y)
+      fun (mi, ma) a' ->
+        (min mi a', max ma a')
+    ) (Float.max_float, 0.) a in
+    let (min_x, max_x) = min_max x in
+    let (min_y, max_y) = min_max y in
+    (min_x, max_x, min_y, max_y)
+
+  let get_min_max l =
+    List.fold_left (
+      fun (min_x, max_x, min_y, max_y) xy ->
+        let (min_x', max_x', min_y', max_y') = get_xy_min_max xy in
+        (min min_x min_x', max max_x max_x', min min_y min_y', max max_y max_y')
+    ) (Float.max_float, 0., Float.max_float, 0.) l
+
+  let fill l =
+    List.iter (
+      fun (x, y) -> plfill x y;
+    ) l
+
+  let outline l =
+    List.iter (
+      fun (x, y) -> plline x y;
+    ) l
 
   let draw p () =
-    let (x, y) = get_xy p in
-    let (xmax, xmin, ymax, ymin) = get_xy_min_max (x, y) in
+    let l = get_xy p in
+    let (xmin, xmax, ymin, ymax) = get_min_max l in
 
     plparseopts Sys.argv [PL_PARSE_FULL];
 
@@ -62,7 +73,11 @@ module Map = struct
 
     plenv xmin xmax ymin ymax 0 0 ;
 
-    plfill x y;
+    plcol0 5;
+    fill l;
+
+    plcol0 10;
+    outline l;
 
     plend ();
 
