dc2c905f94d4ea6c51349333421f3888d7edcd15
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-04-29 00:56:34 +0100

    fixed scalling issue, queries

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -2,11 +2,9 @@ open Backend.Map
 open Backend.Pg
 
 let () =
-  let query = "SELECT st_astext(st_simplifypreservetopology(geom, 60)) FROM  cont_freguesias WHERE distrito_ilha = 'Braga';" in
-  let connection = Map.connect in
-  let p = Map.from_query ~connection ~query in
-
-  Map.draw
+  let p = Map.country_parishes
+    ~connection: Map.connect
+  in Map.draw
     ~polygon: p
     ~name: "plot.svg"
     ~format: "svgcairo"
--- backend/lib/map.ml
+++ backend/lib/map.ml
@@ -16,21 +16,31 @@ module Map = struct
       parse c
     ) r) (fetch_single_result connection)#get_all
 
+  let country_parishes ~connection =
+    from_query ~connection ~query: "SELECT st_astext(st_simplify(geom, 60)) FROM  cont_freguesias;"
+
+  let district_parishes ~district ~connection =
+    let query =
+      Printf.sprintf "SELECT st_astext(st_simplify(geom, 60)) FROM cont_freguesias WHERE distrito_ilha = '%s'"
+        district in
+    from_query ~connection ~query
+
   let get_pol_xy pol =
-    let (x, y) = List.fold_left
-      (fun (acc_x, acc_y) p' ->
-        let x_to_add, y_to_add = List.fold_left
-        (fun (x', y') (x_to_add, y_to_add) ->
-          (x' @ [x_to_add], y' @ [y_to_add])
-        ) ([],[]) p'
-        in (acc_x @ x_to_add, acc_y @ y_to_add)
-      ) ([], []) pol
-    in (Array.of_list x, Array.of_list y)
+    let (x, y) = List.fold_left (
+      fun (acc_x, acc_y) p' ->
+        let x_to_add, y_to_add = List.fold_left (
+          fun (x', y') (x_to_add, y_to_add) ->
+            (x' @ [x_to_add], y' @ [y_to_add])
+        ) ([],[]) p' in
+        (acc_x @ x_to_add, acc_y @ y_to_add)
+      ) ([], []) pol in
+    (Array.of_list x, Array.of_list y)
 
   let get_wkt_xy_l pol =
     match pol with
     | Polygon p ->
       [get_pol_xy p]
+
     | Multipolygon p ->
       List.map (fun p' -> get_pol_xy p') p
 
@@ -41,21 +51,24 @@ module Map = struct
       ) [] p_l)
     ) [] pols_l_l
 
-  let get_xy_min_max (x, y) =
+  let get_min_max_xy (x, y) =
     let min_max a = Array.fold_left (
       fun (mi, ma) a' ->
         (min mi a', max ma a')
     ) (Float.max_float, 0.) a in
+
     let (min_x, max_x) = min_max x in
     let (min_y, max_y) = min_max y in
+
     (min_x, max_x, min_y, max_y)
 
   let get_min_max l =
-    List.fold_left (
+    let (min_x, max_x, min_y, max_y) = List.fold_left (
       fun (min_x, max_x, min_y, max_y) xy ->
-        let (min_x', max_x', min_y', max_y') = get_xy_min_max xy in
+        let (min_x', max_x', min_y', max_y') = get_min_max_xy xy in
         (min min_x min_x', max max_x max_x', min min_y min_y', max max_y max_y')
-    ) (Float.max_float, 0., Float.max_float, 0.) l
+    ) (Float.max_float, 0., Float.max_float, 0.) l in
+    (min min_x min_y, max max_x max_y)
 
   let fill l =
     List.iter (
@@ -69,7 +82,7 @@ module Map = struct
 
   let draw ~polygon ~name ~format () =
     let l = get_xy polygon in
-    let (xmin, xmax, ymin, ymax) = get_min_max l in
+    let (mi, ma) = get_min_max l in
 
     plparseopts Sys.argv [PL_PARSE_FULL];
 
@@ -78,7 +91,7 @@ module Map = struct
 
     plinit ();
 
-    plenv xmin xmax ymin ymax 0 0 ;
+    plenv mi ma mi ma 0 0;
 
     plcol0 5;
     fill l;
