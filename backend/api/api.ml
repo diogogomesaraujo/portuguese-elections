0803133c7a8c615835e299017076f7643a6c1f87
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-05-31 04:16:24 +0100

    working form for map

--- backend/api/api.ml
+++ backend/api/api.ml
@@ -1,3 +1,4 @@
+open Backend.Regions
 open Backend.Map
 open Lwt.Syntax
 open Lwt.Infix
@@ -6,61 +7,110 @@ module Api = struct
   let headers = [("Access-Control-Allow-Origin", "*")]
 
   let res = function
+    | Ok (Some res) ->
+      `String res
+      |> Yojson.to_string
+      |> Dream.response ~headers
+      |> Lwt.return
+    | _ -> Yojson.to_string (`List [])
+      |> Dream.response ~headers
+      |> Lwt.return
+
+  let res_list = function
     | Ok res ->
-      `String (Option.get res)
+      let l =
+        List.map
+          (fun e -> `String e)
+          res
+      in
+      `List l
       |> Yojson.to_string
       |> Dream.response ~headers
       |> Lwt.return
     | Error e -> Yojson.to_string (`List [])
-      |> Dream.response
+      |> Dream.response ~headers
       |> Lwt.return
 
-  let map_req ~connection ~name ~map ~precision =
-    let module Conn = (val connection : Caqti_lwt.CONNECTION) in
-    Dream.get (Printf.sprintf "/%s/:%s" name name)
-      (fun req ->
-        let param = Dream.param req name in
-        let%lwt result = Conn.find_opt (map param ~precision) () in
-        res result
-      )
+  module Regions = struct
+    let regions_req ~name ~arg ~regions =
+      Dream.get (Printf.sprintf "/%s/:%s" name arg)
+        (fun req -> Dream.sql req (fun conn ->
+          let module Conn = (val conn : Caqti_lwt.CONNECTION) in
+          let param = Dream.param req arg in
+          let%lwt result = Conn.collect_list (regions param) () in
+          res_list result)
+        )
 
-  let country_districts ~connection =
-    map_req
-      ~connection
-      ~name: "country"
-      ~map: Map.country_districts
-      ~precision: 5
+    let districts =
+      regions_req
+        ~name: "districts"
+        ~arg:  "arg"
+        ~regions: Regions.districts
 
-  let district_municipalities ~connection =
-    map_req
-      ~connection
-      ~name: "district"
-      ~map: Map.district_municipalities
-      ~precision: 5
+    let municipalities =
+      regions_req
+        ~name: "municipalities"
+        ~arg:  "district"
+        ~regions: Regions.municipalities
 
-  let municipality_parishes ~connection =
-    map_req
-      ~connection
-      ~name: "municipality"
-      ~map: Map.municipality_parishes
-      ~precision: 5
+    let parishes =
+      regions_req
+        ~name: "parishes"
+        ~arg:  "municipality"
+        ~regions: Regions.parishes
+  end
 
-  let parish ~connection =
-    map_req
-      ~connection
-      ~name: "parish"
-      ~map: Map.parish
-      ~precision: 5
+  module Map = struct
+    let map_req ~name ~map ~precision =
+      Dream.get (Printf.sprintf "/%s/:%s" name name)
+        (fun req -> Dream.sql req (fun conn ->
+            let module Conn = (val conn : Caqti_lwt.CONNECTION) in
+            let param = Dream.param req name in
+            let%lwt result = Conn.find_opt (map param ~precision) () in
+            res result
+          )
+        )
 
-  let run ~connection =
+    let country_districts =
+      map_req
+        ~name: "country"
+        ~map: Map.country_districts
+        ~precision: 5
+
+    let district_municipalities =
+      map_req
+        ~name: "district"
+        ~map: Map.district_municipalities
+        ~precision: 5
+
+    let municipality_parishes =
+      map_req
+        ~name: "municipality"
+        ~map: Map.municipality_parishes
+        ~precision: 5
+
+    let parish =
+      map_req
+        ~name: "parish"
+        ~map: Map.parish
+        ~precision: 5
+  end
+
+  let run =
     Dream.run
     @@ Dream.logger
+    @@ Dream.sql_pool "postgresql://localhost:5432/spatial"
     @@ Dream.router [
+      Dream.scope "/regions" [Dream.memory_sessions] [
+        Regions.districts;
+        Regions.municipalities;
+        Regions.parishes;
+      ];
       Dream.scope "/map" [Dream.memory_sessions] [
-        country_districts       ~connection;
-        district_municipalities ~connection;
-        municipality_parishes   ~connection;
-        parish                  ~connection;
+        Map.country_districts;
+        Map.district_municipalities;
+        Map.municipality_parishes;
+        Map.parish;
       ]
     ]
 end
--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,11 +1,5 @@
-open Backend.Pg
 open Api
 open Lwt.Infix
 open Lwt.Syntax
 
-let () =
-  let connection =
-    Lwt_main.run
-      (Pg.connect ~uri: "postgresql://localhost:5432/spatial" ())
-  in
-  Api.run ~connection
+let () = Api.run
