cfdbd62a484665fa58b91b6cb4dc7c196d8eaa5d
Rodrigo Araújo <rodrigogomesaraujo04@gmail.com>
2026-06-03 19:38:29 +0100

    party sitting fixed

--- database/sql/functions/analytics/political_entity_order.sql
+++ database/sql/functions/analytics/political_entity_order.sql
@@ -1,24 +1,32 @@
-CREATE SCHEMA IF NOT EXISTS wh;
-
 CREATE OR REPLACE FUNCTION op.normalized_sigla(p_sigla text)
 RETURNS text
 LANGUAGE sql
 IMMUTABLE
 AS $$
-    SELECT CASE
-        WHEN upper(regexp_replace(COALESCE(p_sigla, ''), '\s+', '', 'g')) IN ('BE', 'B.E')
-            THEN 'B.E.'
-        WHEN upper(regexp_replace(COALESCE(p_sigla, ''), '\s+', '', 'g')) IN ('RIR', 'R.I.R')
-            THEN 'R.I.R.'
-        WHEN upper(regexp_replace(COALESCE(p_sigla, ''), '\s+', '', 'g')) IN ('VOLT', 'VOLT.')
-            THEN 'VP'
-        ELSE replace(
-            upper(regexp_replace(COALESCE(p_sigla, ''), '\s+', '', 'g')),
-            'PPD/PDS',
-            'PPD/PSD'
-        )
-    END;
+WITH s AS (
+    SELECT upper(regexp_replace(COALESCE(p_sigla, ''), '\s+', '', 'g')) AS sigla
+)
+SELECT CASE
+    WHEN (SELECT sigla FROM s) IN ('BE', 'B.E', 'B.E.')
+        THEN 'B.E.'
+
+    WHEN (SELECT sigla FROM s) IN ('RIR', 'R.I.R', 'R.I.R.')
+        THEN 'R.I.R.'
+
+    WHEN (SELECT sigla FROM s) IN ('VOLT', 'VOLT.')
+        THEN 'VP'
+
+    WHEN (SELECT sigla FROM s) = 'PSD'
+        THEN 'PPD/PSD'
+
+    ELSE replace(
+        (SELECT sigla FROM s),
+        'PPD/PDS',
+        'PPD/PSD'
+    )
+END;
 $$;
+
 
 CREATE OR REPLACE FUNCTION op.known_political_party_order()
 RETURNS TABLE (
@@ -33,7 +41,9 @@ AS $$
         ('PCP-PEV', 10::numeric, '(^|[.+/\-])(PCP-PEV|CDU)($|[.+/\-])'),
         ('PCTP/MRPP', 15::numeric, '(^|[.+/\-])PCTP/MRPP($|[.+/\-])'),
         ('MAS', 18::numeric, '(^|[.+/\-])MAS($|[.+/\-])'),
-        ('B.E.', 20::numeric, '(^|[.+/\-])(B\.E\.|BE)($|[.+/\-])'),
+
+        ('B.E.', 20::numeric, '(^|[.+/\-])(B\.E\.|B\.E|BE)($|[.+/\-])'),
+
         ('L', 30::numeric, '(^|[.+/\-])L($|[.+/\-])'),
         ('PAN', 35::numeric, '(^|[.+/\-])PAN($|[.+/\-])'),
         ('PS', 40::numeric, '(^|[.+/\-])PS($|[.+/\-])'),
@@ -44,20 +54,27 @@ AS $$
         ('PDR', 52::numeric, '(^|[.+/\-])PDR($|[.+/\-])'),
         ('VP', 55::numeric, '(^|[.+/\-])(VP|VOLT)($|[.+/\-])'),
         ('PLS', 57::numeric, '(^|[.+/\-])PLS($|[.+/\-])'),
-        ('R.I.R.', 62::numeric, '(^|[.+/\-])(R\.I\.R\.|RIR)($|[.+/\-])'),
+        ('R.I.R.', 62::numeric, '(^|[.+/\-])(R\.I\.R\.|R\.I\.R|RIR)($|[.+/\-])'),
         ('ADN', 65::numeric, '(^|[.+/\-])ADN($|[.+/\-])'),
         ('NC', 67::numeric, '(^|[.+/\-])NC($|[.+/\-])'),
         ('ND', 68::numeric, '(^|[.+/\-])ND($|[.+/\-])'),
-        ('PPD/PSD', 70::numeric, '(^|[.+\-])(PPD/PSD|PSD)($|[.+/\-])'),
+
+        ('PPD/PSD', 70::numeric, '(^|[.+/\-])(PPD/PSD|PSD)($|[.+/\-])'),
         ('A', 72::numeric, '(^|[.+/\-])A($|[.+/\-])'),
-        ('IL', 75::numeric, '(^|[.+/\-])IL($|[.+/\-])'),
         ('CDS-PP', 80::numeric, '(^|[.+/\-])(CDS-PP|CDS/PP|CDS)($|[.+/\-])'),
         ('PPV/CDC', 82::numeric, '(^|[.+/\-])PPV/CDC($|[.+/\-])'),
         ('PPM', 85::numeric, '(^|[.+/\-])PPM($|[.+/\-])'),
+
+        -- IL must be to the right of AD.
+        -- AD = average(PPD/PSD 70, CDS-PP 80, PPM 85) = 78.33
+        -- IL = 87, therefore AD < IL.
+        ('IL', 87::numeric, '(^|[.+/\-])IL($|[.+/\-])'),
+
         ('PNR', 88::numeric, '(^|[.+/\-])PNR($|[.+/\-])'),
         ('CH', 90::numeric, '(^|[.+/\-])CH($|[.+/\-])'),
         ('E', 92::numeric, '(^|[.+/\-])E($|[.+/\-])')
 $$;
+
 
 CREATE OR REPLACE FUNCTION op.detect_political_entity_members(p_sigla text)
 RETURNS TABLE (
@@ -70,6 +87,7 @@ AS $$
 WITH s AS (
     SELECT op.normalized_sigla(p_sigla) AS sigla
 ),
+
 special_ad AS (
     SELECT 'PPD/PSD'::text AS member_sigla, 70::numeric AS member_order
     FROM s
@@ -77,17 +95,28 @@ special_ad AS (
 
     UNION ALL
 
-    SELECT 'CDS-PP'::text, 80::numeric
+    SELECT 'CDS-PP'::text AS member_sigla, 80::numeric AS member_order
     FROM s
     WHERE sigla = 'AD'
 
     UNION ALL
 
-    SELECT 'PPM'::text, 85::numeric
+    SELECT 'PPM'::text AS member_sigla, 85::numeric AS member_order
     FROM s
     WHERE sigla = 'AD'
 ),
-detected AS (
+
+exact_party AS (
+    SELECT
+        k.member_sigla,
+        k.member_order
+    FROM s
+    JOIN op.known_political_party_order() k
+      ON k.member_sigla = s.sigla
+    WHERE s.sigla <> 'AD'
+),
+
+coalition_detected AS (
     SELECT DISTINCT
         k.member_sigla,
         k.member_order
@@ -95,16 +124,36 @@ detected AS (
     JOIN op.known_political_party_order() k
       ON s.sigla ~ k.match_pattern
     WHERE s.sigla <> 'AD'
+      AND s.sigla <> k.member_sigla
+      AND NOT EXISTS (
+          SELECT 1
+          FROM op.known_political_party_order() kk
+          WHERE kk.member_sigla = s.sigla
+      )
+),
+
+combined AS (
+    SELECT member_sigla, member_order
+    FROM special_ad
+
+    UNION
+
+    SELECT member_sigla, member_order
+    FROM exact_party
+
+    UNION
+
+    SELECT member_sigla, member_order
+    FROM coalition_detected
 )
-SELECT member_sigla, member_order
-FROM special_ad
 
-UNION
-
-SELECT member_sigla, member_order
-FROM detected
+SELECT
+    member_sigla,
+    member_order
+FROM combined
 ORDER BY member_order, member_sigla;
 $$;
+
 
 CREATE OR REPLACE FUNCTION op.infer_political_entity_type(
     p_sigla text,
@@ -146,6 +195,7 @@ SELECT CASE
 END;
 $$;
 
+
 CREATE OR REPLACE PROCEDURE op.rebuild_political_entity_members()
 LANGUAGE plpgsql
 AS $$
@@ -175,6 +225,7 @@ BEGIN
     ON CONFLICT DO NOTHING;
 END;
 $$;
+
 
 CREATE OR REPLACE FUNCTION wh.canonical_political_entity_sigla(
     p_election_type text,
@@ -232,10 +283,10 @@ SELECT CASE
      )
         THEN 'AD'
 
-    WHEN (SELECT sigla FROM s) IN ('BE', 'B.E')
+    WHEN (SELECT sigla FROM s) IN ('BE', 'B.E', 'B.E.')
         THEN 'B.E.'
 
-    WHEN (SELECT sigla FROM s) IN ('RIR', 'R.I.R')
+    WHEN (SELECT sigla FROM s) IN ('RIR', 'R.I.R', 'R.I.R.')
         THEN 'R.I.R.'
 
     WHEN (SELECT sigla FROM s) IN ('VOLT', 'VOLT.')
@@ -245,17 +296,22 @@ SELECT CASE
 END;
 $$;
 
+
 CREATE OR REPLACE FUNCTION wh.political_entity_order(p_sigla text)
 RETURNS numeric
 LANGUAGE sql
 IMMUTABLE
 AS $$
-    SELECT COALESCE(
-        (SELECT AVG(member_order) FROM op.detect_political_entity_members(p_sigla)),
-        CASE
-            WHEN op.normalized_sigla(p_sigla) = 'BRANCOS' THEN 1000::numeric
-            WHEN op.normalized_sigla(p_sigla) = 'NULOS' THEN 1001::numeric
-            ELSE 999::numeric
-        END
-    );
+WITH detected AS (
+    SELECT member_order
+    FROM op.detect_political_entity_members(p_sigla)
+)
+SELECT COALESCE(
+    (SELECT AVG(member_order) FROM detected),
+    CASE
+        WHEN op.normalized_sigla(p_sigla) = 'BRANCOS' THEN 1000::numeric
+        WHEN op.normalized_sigla(p_sigla) = 'NULOS' THEN 1001::numeric
+        ELSE 999::numeric
+    END
+);
 $$;
